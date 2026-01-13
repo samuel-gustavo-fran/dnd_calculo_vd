892dca6fa237d741bd0634bd5c76685875dc9909
samuel-gustavo-fran <samucagustavo22@gmail.com>
2026-01-12 21:36:47 -0300

    correção de return
    em caso multeclasse ele iria retornar a vida de cada classe individualmente

--- funçao dnd_calculo_vd.py
+++ funçao dnd_calculo_vd.py
@@ -1,3 +1,4 @@
 def dnd_calculo_vd(ldvida,lnivel,mcon):
     lista_de_calculode_vida = [((((i/2)+1)+mcon)*(j - 1))+(i+mcon) if ldvida[0] == i else (((i/2)+1)+mcon)*j  for i , j in zip(ldvida,lnivel)]
-    return lista_de_calculode_vida
+    return sum(lista_de_calculode_vida)
+    
